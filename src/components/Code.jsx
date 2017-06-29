3f73d139ee5b772459c81b1af43cf720dcfee782
j- <j@skeoh.com>
2017-06-29 20:49:13 +1000

    Style code output

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Card } from 'elemental';
 
 const Code = ({ location, innerWidth, innerHeight }) => (
-	<pre>{`window.open("${location}", null, "width=${innerWidth},height=${innerHeight}")`}</pre>
+	<Card style={{ backgroundColor: '#1e1e1e', wordWrap: 'break-word' }}>
+		<code style={{ color: '#9cdcfe' }}>window</code>
+		<code style={{ color: '#d4d4d4' }}>.</code>
+		<code style={{ color: '#dcdcaa' }}>open</code>
+		<code style={{ color: '#d4d4d4' }}>(</code>
+		<code style={{ color: '#ce9178' }}>{`'${location}'`}</code>
+		<code style={{ color: '#d4d4d4' }}>{', '}</code>
+		<code style={{ color: '#569cd6' }}>null</code>
+		<code style={{ color: '#d4d4d4' }}>{', '}</code>
+		<code style={{ color: '#ce9178' }}>{`'width=${innerWidth},height=${innerHeight}'`}</code>
+		<code style={{ color: '#d4d4d4' }}>);</code>
+	</Card>
 );
 
 export default Code;
