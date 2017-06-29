5ca9f7ce6e39a3f2aa30dc9981614dd8f1e8f40c
j- <j@skeoh.com>
2017-06-29 21:11:27 +1000

    Add code component stylesheet

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Card } from 'elemental';
+import './Code.less';
 
 const Code = ({ location, innerWidth, innerHeight }) => (
 	<Card style={{ backgroundColor: '#1e1e1e', wordWrap: 'break-word' }}>
-		<code style={{ color: '#9cdcfe' }}>window</code>
-		<code style={{ color: '#d4d4d4' }}>.</code>
-		<code style={{ color: '#dcdcaa' }}>open</code>
-		<code style={{ color: '#d4d4d4' }}>(</code>
-		<code style={{ color: '#ce9178' }}>{`'${location}'`}</code>
-		<code style={{ color: '#d4d4d4' }}>{', '}</code>
-		<code style={{ color: '#569cd6' }}>null</code>
-		<code style={{ color: '#d4d4d4' }}>{', '}</code>
-		<code style={{ color: '#ce9178' }}>{`'width=${innerWidth},height=${innerHeight}'`}</code>
-		<code style={{ color: '#d4d4d4' }}>);</code>
+		<code className="mtk11">window</code>
+		<code className="mtk1">.</code>
+		<code className="mtk10">open</code>
+		<code className="mtk1">(</code>
+		<code className="mtk7 hint" title="The URL to be loaded in the newly opened window.">{`'${location}'`}</code>
+		<code className="mtk1">{', '}</code>
+		<code className="mtk5 hint" title="A name for the new window.">null</code>
+		<code className="mtk1">{', '}</code>
+		<code className="mtk7 hint" title="An optional parameter listing the features (size, position, scrollbars, etc.) of the new window.">{`'width=${innerWidth},height=${innerHeight}'`}</code>
+		<code className="mtk1">);</code>
 	</Card>
 );
 
